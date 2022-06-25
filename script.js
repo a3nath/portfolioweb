fea2734f6d4747a8169433e7b18535553a445fd7
a3nath <amarn.sks@gmail.com>
2022-06-24 21:46:38 -0400

    rotate bgd icons on hover

--- script.js
+++ script.js
@@ -6,8 +6,24 @@ function pauseVideo(e){
     this.pause()
 }
 
-const projvid = document.querySelectorAll('.projvid');
+function playRotate(e){
+    //add property
+    // animation: spin 8s linear infinite;
+    this.style.animation = "spin 4s linear infinite"
+}
 
-projvid.forEach(vid => vid.addEventListener("mouseover", playVideo))
+function pauseRotate(e){
+    //add property
+    // animation: spin 8s linear infinite;
+    this.style.animation = ""
+}
 
-projvid.forEach(vid => vid.addEventListener("mouseleave", pauseVideo))
+const projVid = document.querySelectorAll('.projvid');
+
+projVid.forEach(vid => vid.addEventListener("mouseover", playVideo))
+projVid.forEach(vid => vid.addEventListener("mouseleave", pauseVideo))
+
+const svgImg = document.querySelectorAll('.svg-img');
+
+svgImg.forEach(img => img.addEventListener("mouseover", playRotate))
+svgImg.forEach(img => img.addEventListener("mouseleave", pauseRotate))
